55d5ab8bfc2e245e2b41ce59ecac1549811e66f6
yohandal123 <yohandalphatadu@rocketmail.com>
2023-06-19 11:25:45 +0530

    SS

--- config/app.php
+++ config/app.php
@@ -181,6 +181,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Darryldecode\Cart\CartServiceProvider::class,
 
         /*
          * Package Service Providers...
@@ -194,8 +195,6 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        Darryldecode\Cart\CartServiceProvider::class,
-
     ],
 
     /*
