1fe27b43fc52e383c42efc8728ea6563916ae292
wiesinghe86 <wijesinghenilusha@yahoo.com>
2023-06-25 18:46:48 +0530

    rawMaterialIssuance2

--- resources/views/pages/RawMaterialIssueForProduction/index.blade.php
+++ resources/views/pages/RawMaterialIssueForProduction/index.blade.php
@@ -9,7 +9,7 @@
                     <h4>Raw Material Issue For Production List</h2>
                         <div class="d-grid gap-2 d-md-flex justify-content-md-end">
                         <a href="{{ route('rawmaterialissueforproduction.create') }}" class="btn btn-success float-end mb-2"> Add New </a>
-
+                        </div>
                         <table class="table table-bordered" id="tbl_rawmaterialissueforproduction">
                             <thead>
                                 <tr>
@@ -22,7 +22,7 @@
                                     <td>Weight</td>
                                    <td>Location</td>
                                    <td>Issued by</td>
-                                    
+
                                 </tr>
                             </thead>
 
@@ -32,5 +32,6 @@
         </div>
     </div>
 </div>
+
 @endsection
 
