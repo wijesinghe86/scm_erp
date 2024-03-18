29c181abe284094d61575a189ec60ba88adea3a1
wiesinghe86 <wijesinghenilusha@yahoo.com>
2024-03-18 11:00:01 +0530

    BalanceOrderPrint1803

--- resources/views/pages/BalanceOrder/pdf.blade.php
+++ resources/views/pages/BalanceOrder/pdf.blade.php
@@ -8,10 +8,10 @@
         table {
             width: 103mm;
             /* border-collapse: collapse; */
-            border: 1px solid black;
+            /* border: 1px solid black; */
             margin: 0;
             padding: 0;
-            font-size: 10px;
+            font-size: 13px;
         }
 
         th {
@@ -20,38 +20,41 @@
 
         td,
         th {
-            border: 1px solid black;
+             /* border: 1px solid black;  */
         }
     </style>
     <title>{{ $balance_order->balance_order_no }}</title>
 </head>
 
 <body>
-    <table style="height: 21mm;">
+
+    {{-- <table style="height: 21mm;">
         <tr>
             <td></td>
             <td></td>
         </tr>
-    </table>
+    </table> --}}
+    <p style="text-align: left; text-decoration:underline">Balance Order</p>
     <table>
         <tr>
             <td style="height:9mm;width: 13mm;">Date:</td>
             <td style="height:9mm;width: 100%;">{{ date('Y-m-d', strtotime($balance_order->created_at)) }}</td>
-            <td style="height:9mm;">Location</td>
+            <td style="height:9mm;">Location:</td>
             <td style="height:9mm;width: 12mm;">{{ $balance_order->location->warehouse_name }}</td>
         </tr>
     </table>
     <table>
         <tr>
-            <td style="width: 13mm;">
+            <td style="width: 20mm;">
                 Customer:
             </td>
             <td colspan="5">
                 {{ $balance_order->invoice->customer->customer_name }}</br>
-                {{ $balance_order->invoice->customer->customer_mobile_number }}</br>
-                {{ $balance_order->invoice->customer->customer_email }}</br>
+                {{-- {{ $balance_order->invoice->customer->customer_mobile_number }}</br>
+                {{ $balance_order->invoice->customer->customer_email }}</br> --}}
             </td>
         </tr>
+        <br>
         <tr>
             <td>
                 Invoice No.
@@ -62,28 +65,30 @@
             <td>BO No.</td>
             <td>{{ $balance_order->balance_order_no }}</td>
         </tr>
+        <br>
+        <br>
     </table>
     <table style="height: 83mm;">
         <tr>
             <th style="width: 7mm;">No.</th>
             <th style="width: 19mm;">Stock No</th>
-            <th style="width: 44mm;">Description</th>
-            <th style="width: 13mm;">U/M</th>
-            <th style="width: 20mm;">Quantity</th>
+            <th style="width: 60mm;">Description</th>
+            <th style="width: 15mm;">U/M</th>
+            <th style="width: 13mm;">Quantity</th>
         </tr>
         @foreach ($balance_order->items as $key => $item)
             <tr>
                 <td style="width: 7mm;">{{ $key + 1 }}</td>
                 <td style="width: 19mm;">{{ $item->stock_no }}</td>
-                <td style="width: 44mm;">{{ $item->description }}</td>
-                <td style="width: 13mm;">{{ $item->uom }}</td>
+                <td style="width: 60mm;">{{ $item->description }}</td>
+                <td style="width: 15mm;">{{ $item->uom }}</td>
                 <td style="width: 20mm;">{{ $item->qty }}</td>
             </tr>
         @endforeach
     </table>
     <table>
         <tr>
-            <td>{{ $balance_order->created_by }}</td>
+            <td>Created By:{{ $balance_order->createUser->name }}</td>
         </tr>
     </table>
 </body>
